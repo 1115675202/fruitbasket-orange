9a2cf0fd2723721e187f8b94a854252168009a00
1115675202 <1050794513@qq.com>
2020-12-18 14:24:16 +0800

    refactor(架构): spy sql 格式化输出

---
+++ src/main/java/com/example/spring/boot/zhaoyun/module/common/api/am/IBaseVO.java
@@ -0,0 +1,38 @@
+package com.example.spring.boot.zhaoyun.module.common.api.am;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import java.time.LocalDateTime;
+
+/**
+ * 基础返回视图接口
+ *
+ * @author LiuBing
+ * @date 2020/12/16
+ */
+public interface IBaseVO {
+
+	/**
+	 * 主键
+	 *
+	 * @return
+	 */
+	@ApiModelProperty(value = "主键")
+	Integer getId();
+
+	/**
+	 * 创建时间
+	 *
+	 * @return
+	 */
+	@ApiModelProperty(value = "创建时间")
+	LocalDateTime getGmtCreate();
+
+	/**
+	 * 最后修改时间
+	 *
+	 * @return
+	 */
+	@ApiModelProperty(value = "最后修改时间")
+	LocalDateTime getGmtModified();
+}
